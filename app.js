869a4cf2a6c7cc89539e813ef2b98421494104de
Stefen Cabusas <scabusas@sg.ibm.com>
2018-03-05 22:43:10 +0800

    Add body parser and cors

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors');
 var db = require('./db');
+
+var corsOptions = {
+  origin: '*',
+  exposedHeaders: ['Content-Range', 'X-Content-Range', 'Content-Disposition', 'Content-Error', 'X-Powered-By'],
+  credentials: false
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 var ReviewsController = require('./reviewsController');
 app.use('/reviews', ReviewsController);
