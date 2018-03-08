5040268b78e33a18200576c319bb8e4fabbfa138
Stefen Cabusas <scabusas@sg.ibm.com>
2018-03-08 19:44:38 +0800

    merge sin and added previous counts

--- reviews.js
+++ reviews.js
@@ -2,12 +2,16 @@ var mongoose = require('mongoose');
 var reviewSchema = new mongoose.Schema({  
   asin: String,
   review_count: String,
+  review_count2: String,
+  review_count3: String,
   name: String,
   ratings: Object,
   url: String,
   price: String,
   ts: Date,
-  overall_rating: String
+  overall_rating: String,
+  overall_rating2: String,
+  overall_rating3: String
 });
 mongoose.model('Review', reviewSchema);
 module.exports = mongoose.model('Review');
--- reviewsController.js
+++ reviewsController.js
@@ -5,23 +5,96 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 var Reviews = require('./reviews');
 
-router.get('/', function (req, res) {
-  Reviews.find({}, function (err, reviews) {
-      if (err) return res.status(500).send("There was a problem finding the users.");
-      res.status(200).send(reviews);
+// router.get('/', function (req, res) {
+//   Reviews.find({}, function (err, reviews) {
+//       if (err) return res.status(500).send("There was a problem finding the users.");
+//       res.status(200).send(reviews);
+//   });
+// });
+
+router.get('/asin/:asin', function (req, res) {
+  var query = Reviews.find({'asin':req.params.asin}).sort('-ts').limit(5);
+  query.exec(function (err, reviews) {
+    if (err) return res.status(500).send("There was a problem finding the asin.");
+    res.status(200).send(reviews);
   });
 });
 
-router.get('/asin/:asin', function (req, res) {
+router.get('/all', function (req, res) {
+  const allAsins = [
+    "B073C4H9RT",
+    "B07373WJ5T",
+
+    // "B072JBC1GM",
+    // "B07144XG5Q",
+    // "B072HMPPH2",
+    // "B01FRGGNCU",
+    // "B01NBRB8B6",
+    // "B01N1416SN",
+
+    // "B074V4CJJK",
+    // "B073NN58DQ",
+    // "B073KLVNVX",
+    // "B073DCW3WR",
+    // "B077J56SB7",
+    // "B077J5X5NZ",
+
+    // "B010PB26Z0",
+    // "B01LVZC319",
+    // "B01LW9L9YR",
+    // "B01GIV6E4A",
+    // "B01GJ1NEGK",
+    // "B01GJ1NFSW",
+    // "B01GJ1N9V0",
+    // "B01GJ1NEEM",
+    // "B01GJ1ND5C",
+    // "B01F7RKTII",
+    // "B00ZQDJTOE",
+
+    // "B0761PRC72",
+    // "B075SWX5W8",
+    // "B075SS5H1C",
+    // "B075SZJD5Y",
+  ]
+
+  // for (var asin of allAsins) {
+  //   console.log(asin);
+  //   var query = Reviews.find({'asin':asin}).sort('-ts').limit(5);
+  //   query.exec(function (err, reviews) {
+  //     if (err) return console.log("There was a problem finding the asin.");
+      
+  //     // res.status(200).send(reviews);
+  //   });
+  // }
   // Reviews.find({'asin':req.params.asin}, function (err, reviews) {
   //     if (err) return res.status(500).send("There was a problem finding the users.");
   //     res.status(200).send(reviews);
   // });
 
-  var query = Reviews.find({'asin':req.params.asin}).sort('-ts');
+  var query = Reviews.find({}).sort('-ts').limit(100);
   query.exec(function (err, reviews) {
-    if (err) return res.status(500).send("There was a problem finding the users.");
-      res.status(200).send(reviews);
+    if (err) return res.status(500).send("There was a problem finding the asin.");
+    
+    let tmpReviewObj = {}
+    reviews.forEach(rev => {
+      if(tmpReviewObj[rev.asin]){
+        if(tmpReviewObj[rev.asin].overall_rating2 && tmpReviewObj[rev.asin].review_count2){
+          if(tmpReviewObj[rev.asin].overall_rating3 && tmpReviewObj[rev.asin].review_count3){
+            return;
+          }
+          tmpReviewObj[rev.asin].overall_rating3 = rev.overall_rating;
+          tmpReviewObj[rev.asin].review_count3 = rev.review_count;
+          return;
+        }
+        tmpReviewObj[rev.asin].overall_rating2 = rev.overall_rating;
+        tmpReviewObj[rev.asin].review_count2 = rev.review_count;
+        return;
+      }
+      tmpReviewObj[rev.asin] = rev
+    })
+    console.log(tmpReviewObj);
+    var newReviewsArr = Object.values(tmpReviewObj);
+    res.status(200).send(newReviewsArr);
   });
 });
 
